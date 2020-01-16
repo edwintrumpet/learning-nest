108a5e9eb9cb18787c8e64319b8c9b544d941642
edwin <spark.com.co@gmail.com>
2020-01-16 11:14:43 -0500

    protect routes with guards

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,10 +6,12 @@ import {
   Body,
   Patch,
   Delete,
-  ParseIntPipe
+  ParseIntPipe,
+  UseGuards
 } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "./user.entity";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("users")
 export class UserController {
@@ -21,6 +23,7 @@ export class UserController {
     return user;
   }
 
+  @UseGuards(AuthGuard())
   @Get()
   async getUsers(): Promise<User[]> {
     const users = await this._userService.getAll();
