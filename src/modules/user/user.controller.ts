5236d5fe1bd0ac938927341dee95bc75f9a29302
edwin <spark.com.co@gmail.com>
2020-01-16 09:54:37 -0500

    auth module configuration

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -10,7 +10,6 @@ import {
 } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "./user.entity";
-import { UserDto } from "./dto/user.dto";
 
 @Controller("users")
 export class UserController {
